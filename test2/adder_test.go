f1622f02fadc989bd7c6665ad36a463ea9c4d5aa
Uttej Immadi <immadiuttej@gmail.com>
2022-01-29 03:26:35 +0530

    adding more tests

---
+++ test2/adder_test.go
@@ -0,0 +1,25 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+
+	sum := Add(2, 3)
+	expected := 5
+
+	if sum != expected {
+
+		t.Errorf("expected %d but got %d", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+
+	sum := Add(1, 5)
+
+	fmt.Println(sum)
+	//Output: 6
+}
