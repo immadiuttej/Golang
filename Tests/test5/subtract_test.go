9d4cb15cf847d0058b52433dd7076c458caef23a
Uttej Immadi <immadiuttej@gmail.com>
2022-02-01 02:14:18 +0530

    adding test5

---
+++ Tests/test5/subtract_test.go
@@ -0,0 +1,38 @@
+package subtract
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSubtract(t *testing.T) {
+
+	got := Subtract(4, 3)
+	want := 1
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestAbsoluteDifference(t *testing.T) {
+
+	got := AbsoluteDifference([]int{3, 4})
+	want := 1
+
+	if got != want {
+
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestAbsoluteSlices(t *testing.T) {
+
+	got := AbsoluteSlices([]int{3, 4}, []int{5, 9})
+	want := []int{1, 4}
+
+	if !reflect.DeepEqual(got, want) {
+
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
