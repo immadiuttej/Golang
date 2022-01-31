0d385c54853fe327176e9d23ce9ef4fa82208182
Uttej Immadi <immadiuttej@gmail.com>
2022-02-01 02:25:25 +0530

    added an extra testcase to cover 100% of the code

--- Tests/test5/subtract_test.go
+++ Tests/test5/subtract_test.go
@@ -27,12 +27,25 @@ func TestAbsoluteDifference(t *testing.T) {
 }
 
 func TestAbsoluteSlices(t *testing.T) {
+	t.Run("Running with negative difference", func(t *testing.T) {
 
-	got := AbsoluteSlices([]int{3, 4}, []int{5, 9})
-	want := []int{1, 4}
+		got := AbsoluteSlices([]int{3, 4}, []int{5, 9})
+		want := []int{1, 4}
 
-	if !reflect.DeepEqual(got, want) {
+		if !reflect.DeepEqual(got, want) {
 
-		t.Errorf("got %v, want %v", got, want)
-	}
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("running with positive difference", func(t *testing.T) {
+		got := AbsoluteSlices([]int{4, 3}, []int{9, 5})
+		want := []int{1, 4}
+
+		if !reflect.DeepEqual(got, want) {
+
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
 }
